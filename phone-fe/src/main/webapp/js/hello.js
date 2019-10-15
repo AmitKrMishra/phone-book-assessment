18b4ede4b2db1e544a3e24297bda9e20508253b0
AmitKrMishra <amitmishraa3@gmail.com>
2019-10-15 12:47:34 +0200

    Updated script json object parse code

--- phone-fe/src/main/webapp/js/hello.js
+++ phone-fe/src/main/webapp/js/hello.js
@@ -1,16 +1,27 @@
+function addFunction1() {
+		console.log("Inside addFunction");
+		var pname = $('#pname').val();
+		var pnumber = $('#pnumber').val();
+		console.log("pname:"+pname+" pnumber:"+pnumber);	
+}
+function viewFunction() {			
+console.log("Inside viewFunction");
 $(document).ready(function() {
 	var displayResources = $("#display-resources");
     displayResources.text("Loading data from JSON source...");
     $.ajax({
-        url: "http://localhost:8080/all/phone/info"
+        url: "http://localhost:9000/all/phone/info",
+		dataType: 'application/json',
+		
     }).then(function(data) {
     	 console.log(data);
-       var output ="<table><thead><tr><th>Name</th><th>PhoneNumber</th><th>URL</th></thead><tbody>";
-         for (var i in data) {
-              output += "<tr><td>" + data[i].phone +"</td><td>" + data[i].phone +"</td></tr>";
+		 var output ="<table><thead><tr><th>Name</th><th>PhoneNumber</th></thead><tbody>";
+         for (var i in data.phone) {
+              output += "<tr><td>" + data.phone[i].name +"</td><td>" + data.phone[i].phoneNumber +"</td></tr>";
          }
          output += "</tbody></table>";
-         displayResources.html(output);
-         $("table").addClass("table");
+		 displayResources.html(output);
+        
     });
 });
+}
